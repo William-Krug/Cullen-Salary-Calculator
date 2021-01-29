cc6657c338ca3f629bb9c3224688118c89cf522c
William Krug <william.p.krug@gmail.com>
2021-01-29 16:10:16 -0600

    connected "Add Employee" button to JS

--- scripts/client.js
+++ scripts/client.js
@@ -2,8 +2,18 @@ $(document).ready(onReady);
 
 console.log('We have JavaScript');
 
+const verbose = true;
+
 const employees = [];
 
 function onReady() {
   console.log('We have jQuery');
+  $(document).on('click', '#addEmpSubmitBtn', addEmployee);
+}
+
+function addEmployee(event) {
+  event.preventDefault();
+  if (verbose) {
+    console.log('in addEmployee()');
+  }
 }
